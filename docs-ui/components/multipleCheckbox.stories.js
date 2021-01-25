f86398c81847803ca052810171d9cd798d9ed248
Scott Cooper <scttcper@gmail.com>
2021-01-25 15:38:57 -0800

    remove withInfo for storybook

---
+++ docs-ui/components/multipleCheckbox.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {action} from '@storybook/addon-actions';
+
+import MultipleCheckbox from 'app/views/settings/components/forms/controls/multipleCheckbox';
+
+export default {
+  title: 'Core/Forms/Controls',
+};
+
+export const _MultipleCheckbox = () => (
+  <MultipleCheckbox
+    choices={[
+      ['foo', 'Foo'],
+      ['bar', 'Bar'],
+      ['baz', 'Baz'],
+    ]}
+    value={['bar']}
+    onChange={(v, e) => {
+      action('MultipleCheckbox change')(v, e);
+    }}
+  />
+);
+
+_MultipleCheckbox.storyName = 'MultipleCheckbox';
+_MultipleCheckbox.parameters = {
+  docs: {
+    description: {
+      story: 'Multiple Checkbox Control (controlled only atm)',
+    },
+  },
+};
